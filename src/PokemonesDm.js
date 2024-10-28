ca00e468db18ea8a0d8d073f58fa421301393541
Pau <blancopaula48@gmail.com>
2024-10-28 16:53:03 -0500

    prueba

--- src/PokemonesDm.js
+++ src/PokemonesDm.js
@@ -1,33 +1,30 @@
-import { LitElement} from 'lit-element';
-
+import { LitElement } from 'lit-element';
 
 export class PokemonesDm extends LitElement {
-
-    async fetchPokemon() {
+  async fetchPokemon() {
     try {
       const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=50');
-
-      if (!response.ok) {
-        throw new Error(`Error ${response.status}: ${response.statusText}`);
-      }
+      if (!response.ok) throw new Error(`Error ${response.status}: ${response.statusText}`);
 
       const data = await response.json();
-      const pokemonList = await Promise.all(data.results.map(async (pokemon) => {
-        const res = await fetch(pokemon.url);
-        const pokemonDetails = await res.json();
-        return {
-          nombre: pokemonDetails.name,
-          imagen: pokemonDetails.sprites.front_default,
-          tipos: pokemonDetails.types.map(typeInfo => typeInfo.type.name).join(', '),
-        };
-      }));
+      const pokemonList = await Promise.all(
+        data.results.map(async (pokemon) => {
+          const res = await fetch(pokemon.url);
+          const pokemonDetails = await res.json();
+          return {
+            nombre: pokemonDetails.name,
+            imagen: pokemonDetails.sprites.front_default,
+            tipos: pokemonDetails.types.map((typeInfo) => typeInfo.type.name).join(', '),
+          };
+        })
+      );
 
-      this.pokemonData = pokemonList;
-      this.loading = false;
+      return pokemonList; // Cambiado para devolver los datos
     } catch (error) {
       console.error('Error al obtener datos de Pokémon:', error);
-      this.loading = false;
+      return [];
     }
   }
-  
 }
+
+customElements.define('pokemones-dm', PokemonesDm);
