e341dd6c25a34b0292b693453b5685b8d741365a
Pau <blancopaula48@gmail.com>
2024-10-28 17:12:50 -0500

    evoluciones-dm

--- src/PokemonesDm.js
+++ src/PokemonesDm.js
@@ -17,7 +17,6 @@ export class PokemonesDm extends LitElement {
         };
       }));
 
-      // Emitir el evento con los datos cargados
       this.dispatchEvent(new CustomEvent('pokemon-data-loaded', {
         detail: pokemonList,
         bubbles: true,
@@ -30,6 +29,63 @@ export class PokemonesDm extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    this.fetchPokemon(); // Llamar a la carga de datos cuando el componente se conecte
+    this.fetchPokemon(); 
   }
+
+
+  async fetchPokemonDetails(pokemonId) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`);
+      const data = await response.json();
+      const pokemonDetails = {
+        name: data.name,
+        image: data.sprites.front_default,
+        types: data.types.map(typeInfo => typeInfo.type.name).join(', '),
+      };
+
+      const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`);
+      const speciesData = await speciesResponse.json();
+      const evolutionChainUrl = speciesData.evolution_chain.url;
+
+      const evolutionResponse = await fetch(evolutionChainUrl);
+      const evolutionData = await evolutionResponse.json();
+      const evolutions = await this.extractEvolutionsWithImages(evolutionData.chain);
+
+      const noEvolutionsMessage = evolutions.length === 0 ? 'Este Pokémon no tiene evoluciones.' : '';
+
+      this.dispatchEvent(new CustomEvent('pokemon-details-loaded', {
+        detail: { pokemonDetails, evolutions, noEvolutionsMessage },
+        bubbles: true,
+        composed: true,
+      }));
+    } catch (error) {
+      console.error('Error al obtener detalles del Pokémon:', error);
+    }
+  }
+
+  async extractEvolutionsWithImages(chain) {
+    const evolutions = [];
+    let current = chain;
+
+    while (current) {
+      const evolutionName = current.species.name;
+      const evolutionImage = await this.fetchPokemonImage(evolutionName);
+      evolutions.push({ name: evolutionName, image: evolutionImage });
+      current = current.evolves_to[0];
+    }
+
+    return evolutions;
+  }
+
+  async fetchPokemonImage(evolutionName) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${evolutionName}/`);
+      const data = await response.json();
+      return data.sprites.front_default;
+    } catch (error) {
+      console.error(`Error al obtener imagen de ${evolutionName}:`, error);
+      return '';
+    }
+  }
+
 }
