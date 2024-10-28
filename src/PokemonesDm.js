350cb805332d021b0aafab532295dc6ee565780f
Pau <blancopaula48@gmail.com>
2024-10-28 16:23:00 -0500

    prueba

--- src/PokemonesDm.js
+++ src/PokemonesDm.js
@@ -1,12 +1,17 @@
-import { LitElement } from 'lit-element';
+import { LitElement} from 'lit-element';
+
 
 export class PokemonesDm extends LitElement {
 
-  async fetchPokemon() {
+    async fetchPokemon() {
     try {
+      console.log('Iniciando fetch de Pokémon');
       const response = await fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=50');
-      if (!response.ok) throw new Error(`Error ${response.status}: ${response.statusText}`);
-      
+
+      if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+      }
+
       const data = await response.json();
       const pokemonList = await Promise.all(data.results.map(async (pokemon) => {
         const res = await fetch(pokemon.url);
@@ -18,20 +23,12 @@ export class PokemonesDm extends LitElement {
         };
       }));
 
-      // Emitir un evento personalizado con los datos cargados
-      this.dispatchEvent(new CustomEvent('pokemon-data-loaded', {
-        detail: pokemonList,
-        bubbles: true,
-        composed: true
-      }));
+      this.pokemonData = pokemonList;
+      this.loading = false;
     } catch (error) {
       console.error('Error al obtener datos de Pokémon:', error);
+      this.loading = false;
     }
   }
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.fetchPokemon(); // Inicia la carga automáticamente al montar el componente
-  }
+  
 }
-customElements.define('pokemones-dm', PokemonesDm);
